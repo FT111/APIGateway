345ac644be9f905f332bd9feeaf844f660eb4c9c
FT111 <thetaylor2016@gmail.com>
2025-08-09 15:43:53 +0100

    Adds plugin support to Supervisor

---
+++ Supervisor/CoreServiceLoader.cs
@@ -0,0 +1,37 @@
+using GatewayPluginContract;
+
+namespace Supervisor;
+
+public static class CoreServiceLoader
+{
+    public static void LoadFromConfiguration(WebApplicationBuilder builder)
+    {
+        var pluginManager = new PluginManager(builder.Configuration);
+        pluginManager.LoadPluginsAsync("services/plugins");
+        pluginManager.LoadInternalServices("Supervisor.services");
+        
+        var coreServices = builder.Configuration.GetSection("coreServices") 
+            ?? throw new InvalidOperationException("coreServices configuration section not found.");
+        
+        var requiredServices = new[] {"InstanceStore", "MessageAdapter", "SupervisorStore"};
+        Dictionary<string, string> serviceIdentifiers = new Dictionary<string, string>();
+        foreach (var service in requiredServices)
+        {
+            serviceIdentifiers.Add(service, coreServices.GetSection(service)["Identifier"]
+                ?? throw new InvalidOperationException($"Service '{service}' not found in configuration."));
+        }
+
+        var storeFactory = pluginManager.Registrar.GetServiceByName<StoreFactory>(serviceIdentifiers["InstanceStore"]).Instance
+            ?? throw new InvalidOperationException("StoreFactory service not found.");
+        
+        builder.Services.AddSingleton<StoreFactory>(storeFactory);
+        
+        builder.Services.AddScoped<IGatewayRepositories>(sp =>
+        {
+            var store = sp.GetRequiredService<StoreFactory>().CreateStore();
+            return store.GetRepoFactory();
+        });
+        builder.Services.AddSingleton<SupervisorAdapter>(pluginManager.Registrar.GetServiceByName<SupervisorAdapter>(serviceIdentifiers["MessageAdapter"]).Instance
+            ?? throw new InvalidOperationException("SupervisorClient service not found."));
+    }
+}
---
+++ Supervisor/PluginLoader.cs
@@ -0,0 +1,41 @@
+using GatewayPluginContract;
+
+namespace Supervisor;
+
+public static class PluginLoader
+{
+
+    private static System.Collections.IEnumerable GetPluginDlls(string subDirectory)
+    {
+        // var fullDir = Path.Combine(AppContext.BaseDirectory, subDirectory);
+        var fullDir = "/Users/freddietaylor/Projects/C#Stuff/Gateway/Gateway/services/plugins";
+        foreach (var dir in Directory.GetDirectories(fullDir))
+        {
+            var dirName = Path.GetFileName(dir);
+            var targetDll = Path.Combine(dir, dirName + ".dll");
+
+            if (File.Exists(targetDll))
+            {
+                yield return targetDll;
+            }
+        }
+    }
+
+    public static List<McMaster.NETCore.Plugins.PluginLoader> GetPluginLoaders(string subDirectory = "services/plugins")
+    {
+        var pluginLoaders = new List<McMaster.NETCore.Plugins.PluginLoader>();
+        foreach (string pluginDll in GetPluginDlls(subDirectory))
+        {
+            Console.WriteLine($"Loading plugin from {pluginDll}");
+            pluginLoaders.Add(
+                McMaster.NETCore.Plugins.PluginLoader.CreateFromAssemblyFile(
+                    pluginDll,
+                    sharedTypes: new[] { typeof(IPlugin), typeof(GatewayPluginContract.IRequestProcessor), typeof( GatewayPluginContract.IRequestForwarder) },
+                    isUnloadable: true,
+                    configure: config => config.EnableHotReload = true
+                ));
+        }
+        
+        return pluginLoaders;
+    }
+}
