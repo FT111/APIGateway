7363f314494bfe6a0552855986256670b22badca
FT111 <thetaylor2016@gmail.com>
2025-08-23 20:38:46 +0100

    Adds target view models

---
+++ Supervisor/routes/Targets/Models.cs
@@ -0,0 +1,40 @@
+namespace Supervisor.routes.Targets;
+
+public static class Models
+{
+    public class TargetResponse
+    {
+        public Guid Id { get; set; }
+        public string Url { get; set; } = null!;
+        public string Scheme { get; set; } = null!;
+        public string Host { get; set; } = null!;
+        public string? Path { get; set; } = null!;
+        public bool Fallback { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+    
+    public class CreateTargetRequest
+    {
+        public required string Name { get; init; }
+        public required string Scheme { get; init; }
+        public required string Host { get; init; }
+        public string? Path { get; init; }
+        public bool Fallback { get; init; } = false;
+    }
+}
+
+public static class Mapping
+{
+    public static readonly System.Linq.Expressions.Expression<System.Func<GatewayPluginContract.Entities.Target, Models.TargetResponse>> ToResponse = target => new Models.TargetResponse
+    {
+        Id = target.Id,
+        Url = target.Schema + target.Host + target.BasePath,
+        Scheme = target.Schema,
+        Host = target.Host,
+        Path = target.BasePath,
+        Fallback = target.Fallback,
+        CreatedAt = target.CreatedAt,
+        UpdatedAt = target.UpdatedAt
+    };
+}
