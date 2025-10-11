429921e5ecf989add40329d813783f23b48ccfa2
FT111 <thetaylor2016@gmail.com>
2025-10-11 16:31:59 +0100

    Updates dotnet files

---
+++ GatewayPluginContract/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GatewayPluginContract.Entities;
+
+public partial class User : Entity
+{
+    public Guid Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Passwordhs { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
