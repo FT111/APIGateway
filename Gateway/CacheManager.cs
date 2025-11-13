2457b2946db0450f4999e746fc5914438f7cc2a6
FT111 <thetaylor2016@gmail.com>
2025-11-13 11:44:41 +0000

    Seperates cache handler threads with better cancellation support

--- Gateway/CacheManager.cs
+++ Gateway/CacheManager.cs
@@ -4,11 +4,22 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Gateway;
 
-public class CacheManager(StoreFactory stf) : PluginCacheManager(stf)
+public class CacheManager: PluginCacheManager
 {
     private readonly ConcurrentDictionary<string, PluginCache> _caches = new ConcurrentDictionary<string, PluginCache>();
-    private readonly StoreFactory _ctx = stf;
+    private readonly StoreFactory _ctx;
+    private readonly CacheHandler _cacheHandler;
+    private CancellationTokenSource _daemonCancellationTokenSource = new CancellationTokenSource();
 
+    public CacheManager(StoreFactory stf) : base(stf)
+    {
+        _ctx = stf;
+        _cacheHandler = new CacheHandler(stf);
+        
+        // Start the cache daemon
+        Task.Run(() => _cacheHandler.RunAsync(_daemonCancellationTokenSource.Token));
+    }
+    
     public void ConfigurePluginManager(PluginManager pm)
     {
         pm.AddPluginLoadStep(plugin =>
@@ -21,17 +32,21 @@ public class CacheManager(StoreFactory stf) : PluginCacheManager(stf)
     
     public override PluginCache GetCache(string pluginIdentifier)
     {
-        return _caches.GetOrAdd(pluginIdentifier, id => new Cache(_ctx));
+        return _caches.GetOrAdd(pluginIdentifier, id => new Cache(_ctx, _cacheHandler));
+    }
+
+    internal void StopBackgroundRefresh()
+    {
+        _daemonCancellationTokenSource.Cancel();
     }
 }
 
-public class Cache(StoreFactory ctx) : PluginCache(ctx)
+public class Cache(StoreFactory ctx, CacheHandler ch) : PluginCache(ctx, ch)
 {
     private ConcurrentDictionary<string, object> _data = new ConcurrentDictionary<string, object>();
     private ConcurrentDictionary<string, SemaphoreSlim> _locks = new ConcurrentDictionary<string, SemaphoreSlim>();
     private StoreFactory _ctx = ctx;
-
-
+    private CacheHandler _ch = ch;
     
     public override T Get<T>(string key)
     {
@@ -46,7 +61,7 @@ public class Cache(StoreFactory ctx) : PluginCache(ctx)
         {
             throw new InvalidOperationException("Item already exists");
         }
-        _ = AddCacheDaemonAsync(data, key).ConfigureAwait(false);
+        _ch.Register(data, key, _locks[key]);
     }
 
     private async Task AddCacheDaemonAsync<T>(CachedData<T> data, string key) where T : class
@@ -67,8 +82,102 @@ public class Cache(StoreFactory ctx) : PluginCache(ctx)
     }
 }
 
-public class CacheHandler(StoreFactory ctx)
+// Replaces the many cache daemons
+// One central handler that handles all cache invalidation and refetching
+public class CacheHandler(StoreFactory ctx) : ICacheHandler
 {
     private StoreFactory _ctx = ctx;
-    private ConcurrentQueue
+    private ConcurrentQueue<ICacheEntry> _newItems = new ConcurrentQueue<ICacheEntry>();
+    
+    public void Register<T>(CachedData<T> item, string pluginKey,SemaphoreSlim sem) where T : class
+    {
+        var newEntry = new CacheEntry<T>(item, sem, pluginKey);
+        // TODO Fix this being thrown as an invalid cast
+        _newItems.Enqueue(newEntry ?? throw new InvalidOperationException("Type mismatch in cache entry."));
+    }
+    
+    public async Task RunAsync(CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            while (_newItems.TryDequeue(out var obj))
+            {
+                if (cancellationToken.IsCancellationRequested) break;
+                
+                
+                var loop = HandleEntryAsync(obj, obj.CToken);
+            }
+            
+            await Task.Delay(50, cancellationToken);
+        }
+    }
+    
+    private async Task HandleEntryAsync(ICacheEntry entry, CancellationToken cancellationToken)
+    {
+        await entry.RefetchAsync(_ctx, cancellationToken);
+        if (entry.InvalidationFrequency == null) return;
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            try
+            {
+                await Task.Delay(entry.InvalidationFrequency.Value, cancellationToken);
+                await entry.RefetchAsync(_ctx, cancellationToken);
+            }
+            catch (OperationCanceledException)
+            {
+                break;
+            }
+            catch (Exception)
+            {
+                break;
+            }
+        }
+    }
+    
+    // private async Task RefetchEntryAsync(ICacheEntry entry, CancellationToken tok)
+    // {
+    //     await entry.Semaphore.WaitAsync(tok);
+    //     entry.Data = await entry.Data.(_ctx.CreateStore().Context);
+    //     entry.Semaphore.Release();
+    // }
+}
+
+internal interface ICacheEntry
+{
+    SemaphoreSlim Semaphore { get; }
+    object Data { get; set; }
+    string PluginKey { get; }
+    CancellationToken CToken { get; }
+    TimeSpan? InvalidationFrequency { get; }
+    Task RefetchAsync(StoreFactory ctx, CancellationToken tok);
+
+}
+
+internal class CacheEntry<T>(CachedData<T> data, SemaphoreSlim semaphore, string pluginKey) : ICacheEntry  where T : class 
+{
+    CachedData<T> Data { get; } = data;
+    public SemaphoreSlim Semaphore { get; } = semaphore;
+    public string PluginKey { get; } = pluginKey;
+    internal CancellationToken CToken = new CancellationTokenSource().Token;
+    
+    object ICacheEntry.Data
+    {
+        get => Data;
+        set => Data.Data = (T)value;
+    }
+    
+    CancellationToken ICacheEntry.CToken => CToken;
+    TimeSpan? ICacheEntry.InvalidationFrequency => Data.InvalidationFrequency;
+    async Task ICacheEntry.RefetchAsync(StoreFactory ctx, CancellationToken tok)
+    {
+        await Semaphore.WaitAsync(tok);
+        try
+        {
+            Data.Data = await Data.Fetch(ctx.CreateStore().Context);
+        }
+        finally
+        {
+            Semaphore.Release();
+        }
+    }
 }
